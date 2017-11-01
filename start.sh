2ac73b7d91780a35714884a4c6af7f622caaf7cb
Sebastian Struß <sebastian.struss@gmail.com>
2017-11-01 07:44:11 +0100

    manually fixed startup script for scalability of vcsserver

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ service postgresql start
 touch /var/log/rhodecode-vcsserver.log
 touch /var/log/rhodecode-enterprise-ce.log
 cd /rhodecode-develop/rhodecode-vcsserver
-nix-shell --run "pserve configs/production.ini" > /var/log/rhodecode-vcsserver.log &
+nix-shell --run "gunicorn --paster configs/production.ini" > /var/log/rhodecode-vcsserver.log &
 cd /rhodecode-develop/rhodecode-enterprise-ce 
 nix-shell --run "pserve configs/production.ini" > /var/log/rhodecode-enterprise-ce.log &
 tail -f /var/log/rhodecode-*.log
