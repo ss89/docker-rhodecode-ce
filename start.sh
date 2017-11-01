15a882e7395ba008e37e35dac51318615fb0b3e7
Sebastian Struß <sebastian.struss@gmail.com>
2017-11-01 18:37:50 +0100

    manually fixed startup script for scalability of rhodecode-ce

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ touch /var/log/rhodecode-enterprise-ce.log
 cd /rhodecode-develop/rhodecode-vcsserver
 nix-shell --run "gunicorn --paster configs/production.ini" > /var/log/rhodecode-vcsserver.log &
 cd /rhodecode-develop/rhodecode-enterprise-ce 
-nix-shell --run "pserve configs/production.ini" > /var/log/rhodecode-enterprise-ce.log &
+nix-shell --run "gunicorn --paster configs/production.ini" > /var/log/rhodecode-enterprise-ce.log &
 tail -f /var/log/rhodecode-*.log
