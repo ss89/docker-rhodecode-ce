bdd181973f711c3878553fe172d97df2d9a241dd
Sebastian Struß <sebastian.struss@gmail.com>
2018-05-13 20:40:20 +0200

    fixes for nix locale problems

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ export USER=root
 touch /var/log/rhodecode-vcsserver.log
 touch /var/log/rhodecode-enterprise-ce.log
 cd /rhodecode-develop/rhodecode-vcsserver
+export LOCALE_ARCHIVE=`nix-env --installed --no-name --out-path --query glibc-locales`/lib/locale/locale-archive
 nix-shell --run "gunicorn --paster configs/production.ini" > /var/log/rhodecode-vcsserver.log &
 cd /rhodecode-develop/rhodecode-enterprise-ce 
 nix-shell --run "gunicorn --paster configs/production.ini" > /var/log/rhodecode-enterprise-ce.log &
